d7ddd3767276f71a6d0f02695faf10660e387dfc
Manish Mishra <manish.mishra@sierradev.com>
2025-11-08 19:08:51 +0530

    latest1

--- API/Models/Role copy.cs
+++
@@ -1,9 +0,0 @@
-namespace EMS.API.Models
-{
-    public class Role1
-    {
-        public int Id { get; set; }
-        [System.ComponentModel.DataAnnotations.MaxLength(255)]
-        public string Name { get; set; } // Admin | Supervisor
-    }
-}
